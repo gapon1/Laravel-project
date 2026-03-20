9055822aafbd9847579eceeea25bc91760a09397
vitaliihapon <v.gapon@smileexpo.com.ua>
2026-03-19 19:36:38 -0600

    Create User Model

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChirpController;
+use App\Http\Controllers\Auth\Register;
+use App\Http\Controllers\Auth\Logout;
 
-Route::get('/', function () {
-    return view('home');
+Route::get('/', [ChirpController::class, 'index']);
+
+Route::middleware('auth')->group(function () {
+Route::post('/chirps', [ChirpController::class, 'store']);
+Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
+Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
+Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
 });
+
+// Register routes
+Route::view('/register', 'auth/register')
+->middleware('guest')
+->name('register');
+Route::post('/register', Register::class)
+->middleware('guest');
+Route::post('/logout', [Logout::class, '__invoke'])
+->middleware('auth');
+
+//Route::resource('/chirps', ChirpController::class)
+//    ->only(['store', 'edit', 'update', 'destroy']);
